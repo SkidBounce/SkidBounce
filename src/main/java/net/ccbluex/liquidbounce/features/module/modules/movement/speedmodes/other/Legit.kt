876f25ec1b1cff4b6cea2f650ad3ee07f8ccd6e1
ManInMyVan <maninmyvan@gmail.com>
2024-10-20 14:49:00 -0600

    fix legit speed

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/other/Legit.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/other/Legit.kt
@@ -7,16 +7,19 @@ package net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.oth
 
 import net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.SpeedMode
 import net.ccbluex.liquidbounce.utils.MovementUtils.isMoving
-import net.ccbluex.liquidbounce.utils.extensions.jmp
+import net.ccbluex.liquidbounce.utils.extensions.update
 
 /**
  * @author CCBlueX/LiquidBounce
  */
 object Legit : SpeedMode("Legit") {
-    override fun onStrafe() {
-        mc.thePlayer ?: return
+    override fun onUpdate() {
+        if (mc.thePlayer != null && isMoving) {
+            mc.gameSettings.keyBindJump.pressed = true
+        }
+    }
 
-        if (isMoving)
-            mc.thePlayer.jmp()
+    override fun onDisable() {
+        mc.gameSettings.keyBindJump.update()
     }
 }
