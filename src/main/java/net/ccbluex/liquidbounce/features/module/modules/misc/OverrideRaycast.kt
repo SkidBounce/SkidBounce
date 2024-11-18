c0c28c7c14b22e6ce8f0ef70aaed6345675f7ca3
ManInMyVan <maninmyvan@gmail.com>
2024-11-18 10:45:09 -0700

    Update OverrideRaycast.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/OverrideRaycast.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/OverrideRaycast.kt
@@ -1,16 +1,16 @@
 /*
- * LiquidBounce Hacked Client
- * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
- * https://github.com/CCBlueX/LiquidBounce/
+ * SkidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge, Forked from LiquidBounce.
+ * https://github.com/SkidBounce/SkidBounce/
  */
 package net.ccbluex.liquidbounce.features.module.modules.misc
 
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
-import net.ccbluex.liquidbounce.value.BoolValue
+import net.ccbluex.liquidbounce.value.BooleanValue
 
-object OverrideRaycast : Module("OverrideRaycast", Category.MISC, gameDetecting = false, hideModule = false) {
-    private val alwaysActive by BoolValue("AlwaysActive", true)
+object OverrideRaycast : Module("OverrideRaycast", Category.MISC, gameDetecting = false) {
+    private val alwaysActive by BooleanValue("AlwaysActive", true)
 
     fun shouldOverride() = handleEvents() || alwaysActive
 }
