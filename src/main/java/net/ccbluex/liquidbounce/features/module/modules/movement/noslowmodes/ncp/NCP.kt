729c996d1ad82c0729cba5888d94c435b5c239c8
ManInMyVan <maninmyvan@gmail.com>
2024-11-21 20:10:02 -0700

    remove NCP noslow settings
    they are all disabler modes now

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/noslowmodes/ncp/NCP.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/noslowmodes/ncp/NCP.kt
@@ -10,14 +10,11 @@ import net.ccbluex.liquidbounce.event.EventState.PRE
 import net.ccbluex.liquidbounce.event.events.MotionEvent
 import net.ccbluex.liquidbounce.features.module.modules.movement.noslowmodes.NoSlowMode
 import net.ccbluex.liquidbounce.utils.PacketUtils.sendPacket
-import net.ccbluex.liquidbounce.value.BooleanValue
-import net.ccbluex.liquidbounce.value.ListValue
 import net.minecraft.network.play.client.C07PacketPlayerDigging
 import net.minecraft.network.play.client.C07PacketPlayerDigging.Action.RELEASE_USE_ITEM
 import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement
-import net.minecraft.util.BlockPos
 import net.minecraft.util.BlockPos.ORIGIN
-import net.minecraft.util.EnumFacing.byName
+import net.minecraft.util.EnumFacing.DOWN
 
 /**
  * @author CCBlueX/LiquidBounce
@@ -25,29 +22,10 @@ import net.minecraft.util.EnumFacing.byName
  * @author ManInMyVan
  */
 class NCP : NoSlowMode("NCP", swordOnly = true) {
-    private val invalidUsePosition by BooleanValue("InvalidUsePosition", false)
-    private val nullUseItem by BooleanValue("NullUseItem", false)
-    private val invalidReleasePosition by BooleanValue("InvalidReleasePosition", false)
-    private val releaseDirection by ListValue("ReleaseDirection", arrayOf("Down", "Up", "North", "South", "West", "East"), "Down")
-
     override fun onMotion(event: MotionEvent) {
         when (event.eventState) {
-            PRE -> sendPacket(
-                C07PacketPlayerDigging(
-                    RELEASE_USE_ITEM,
-                    if (invalidReleasePosition) BlockPos(-1, -1, -1) else ORIGIN,
-                    byName(releaseDirection.lowercase())
-                )
-            )
-
-            POST -> sendPacket(
-                C08PacketPlayerBlockPlacement(
-                    if (invalidUsePosition) ORIGIN else BlockPos(-1, -1, -1),
-                    255,
-                    if (nullUseItem) null else mc.thePlayer.heldItem,
-                    0f, 0f, 0f
-                )
-            )
+            PRE -> sendPacket(C07PacketPlayerDigging(RELEASE_USE_ITEM, ORIGIN, DOWN))
+            POST -> sendPacket(C08PacketPlayerBlockPlacement(mc.thePlayer.heldItem))
             else -> {}
         }
     }
